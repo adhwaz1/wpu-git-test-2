23fdf9b5f52031ac86c21a43b310da47df928009
adhwaz1 <physico541@gmail.com>
2022-11-02 10:03:58 +0700

    menambakan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus incidunt officiis atque, repudiandae maiores facere voluptate quisquam similique aliquam saepe nemo doloremque, fugit a iusto nisi illo minima, eveniet corrupti!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
